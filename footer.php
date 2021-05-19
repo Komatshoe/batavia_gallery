7e6ad8c0ab48131b5d85bd09c3b719b83bf0b08f
Komatshoe <79903438+Komatshoe@users.noreply.github.com>
2021-05-19 10:03:04 +0800

    Create footer.php

---
+++ footer.php
@@ -0,0 +1,64 @@
+
+    <footer>
+        <div class="container-fluid footer-sec">
+            <div class="container content-footer">
+                <div class="col-md-6 open-hours foot">
+                    <h3 class="body-text title-footer"> OPEN HOURS</h3>
+                    <p class="body-text text-footer">
+                        <br>
+                        Monday 9.30am – 5.00pm <br>
+                        Tuesday 9.30am – 5.00pm <br>
+                        Wednesday 9.30am – 5.00pm <br>
+                        Thursday 9.30am – 5.00pm <br>
+                        Friday 9.30am – 5.00pm <br>
+                        Saturday 9.30am – 5.00pm <br>
+                        Sunday 9.30am – 5.00pm <br>
+                    </p>
+                </div>
+
+                <div class="col-md-2 locaion foot">
+                    <h3 class="body-text title-footer">LOCATION</h3>
+                    <p class="body-text text-footer">
+                        <br>
+                        Shipwrecks Museum <br>
+                        <br>
+                        Cliff Street, <br>
+                        Fremantle, <br>
+                        Western Australia,<br>
+                        6160, <br>
+                        Australia <br>
+                    </p>
+                </div>
+
+
+                <div class="col-md-4 foot">
+                    <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3381.4405397360315!2d115.74053151570645!3d-32.057331434753884!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x2a32a171ad35d0b3%3A0xcd18ab60710a3de8!2sWA%20Shipwrecks%20Museum!5e0!3m2!1sen!2sau!4v1621383030741!5m2!1sen!2sau" width="220" height="220" style="border:0;" allowfullscreen="" loading="lazy"></iframe>
+                </div>
+
+
+
+                <div class="container-fluid col-md-6 logo foot">
+                    <img class="col-xs-3 logos img-responsive" src="http://206.189.45.97/~mesh14/wp-content/themes/batavia_gallery/images/facebook.png">
+                    <img class="col-xs-3 logos img-responsive" src="http://206.189.45.97/~mesh14/wp-content/themes/batavia_gallery/images/instagram.png">
+                    <img class="col-xs-3 logos img-responsive" src="http://206.189.45.97/~mesh14/wp-content/themes/batavia_gallery/images/youtube.png">
+                    <img class="col-xs-3 logos img-responsive" src="http://206.189.45.97/~mesh14/wp-content/themes/batavia_gallery/images/twitter.png">
+                </div>
+
+                <div class="container-fluid col-md-6 foot">
+                    <form class="form-inline" action="/action_page.php">
+                        <div class="col-xs-8 form-group input-footer">
+                            <input type="email" class="form-control" id="email" placeholder="Subsribe to our Newsletter">
+                        </div>
+                        
+                        <div class="col-xs-4 container-fluid button-footer">
+                            <a href="#" class="button-text">JOIN THE CREW</a>
+                        </div>                        
+
+                    </form>
+
+                </div>
+
+            </div>
+        </div>
+    </footer>
+<?php wp_footer(); ?>
