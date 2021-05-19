b65c8196b2180f78ce4650a556332dc76a840b8f
Komatshoe <79903438+Komatshoe@users.noreply.github.com>
2021-05-19 10:03:15 +0800

    Create index.php

---
+++ index.php
@@ -0,0 +1,110 @@
+<?php get_header(); /* Tells WordPress to include header.php */ ?>
+    <section>
+        <div class="container-fluid learn-sec">
+            <div class="container">
+                <h2 class="learn-title"> Learn more about the Batavia</h2>
+
+                <div class="container home-overlap-2">
+
+
+                    <div class="square-1">
+                        <img class="img-responsive" src="http://206.189.45.97/~mesh14/wp-content/themes/batavia_gallery/images/square-1.png">
+                    </div>
+
+                    <div class="large-image-1">
+                        <img class="img-responsive" src="http://206.189.45.97/~mesh14/wp-content/themes/batavia_gallery/images/big-image-1.png">
+                    </div>
+
+                    <div class="small-image-1">
+                        <img class="img-responsive" src="http://206.189.45.97/~mesh14/wp-content/themes/batavia_gallery/images/small-image-1.png">
+                    </div>
+
+                    <div class="title-1">
+                        <h3 class="title-text">New Artifacts</h3>
+                    </div>
+
+                    <div class="text-1">
+                        <p class="body-text">The Batavia Gallery within the WA Shipwrecks Museum has had a substantial upgrade and many artefacts previously held in storage will be exhibited</p>
+                    </div>
+
+                    <div class="container-fluid button-1">
+                        <a href="blog-page.html" class="button-text">Learn More</a>
+                    </div>
+
+
+
+                    <div class="square-2">
+                        <img class="img-responsive" src="http://206.189.45.97/~mesh14/wp-content/themes/batavia_gallery/images/square-1.png">
+                    </div>
+
+                    <div class="large-image-2">
+                        <img class="img-responsive" src="http://206.189.45.97/~mesh14/wp-content/themes/batavia_gallery/images/big-image-2.png">
+                    </div>
+
+                    <div class="small-image-2">
+                        <img class="img-responsive" src="http://206.189.45.97/~mesh14/wp-content/themes/batavia_gallery/images/small-image-2.png">
+                    </div>
+
+                    <div class="title-2">
+                        <h3 class="title-text">New Artifacts</h3>
+                    </div>
+
+                    <div class="text-2">
+                        <p class="body-text">The Batavia Gallery within the WA Shipwrecks Museum has had a substantial upgrade and many artefacts previously held in storage will be exhibited</p>
+                    </div>
+
+                    <div class="container-fluid button-2">
+                        <a href="blog-page.html" class="button-text">Learn More</a>
+                    </div>
+
+
+
+                    <div class="square-3">
+                        <img class="img-responsive" src="http://206.189.45.97/~mesh14/wp-content/themes/batavia_gallery/images/square-1.png">
+                    </div>
+
+                    <div class="large-image-3">
+                        <img class="img-responsive" src="http://206.189.45.97/~mesh14/wp-content/themes/batavia_gallery/images/big-image-3.png">
+                    </div>
+
+                    <div class="small-image-3">
+                        <img class="img-responsive" src="http://206.189.45.97/~mesh14/wp-content/themes/batavia_gallery/images/small-image-3.png">
+                    </div>
+
+                    <div class="title-3">
+                        <h3 class="title-text">New Artifacts</h3>
+                    </div>
+
+                    <div class="text-3">
+                        <p class="body-text">The Batavia Gallery within the WA Shipwrecks Museum has had a substantial upgrade and many artefacts previously held in storage will be exhibited</p>
+                    </div>
+
+                    <div class="container-fluid button-3">
+                        <a href="blog-page.html" class="button-text">Learn More</a>
+                    </div>
+
+                </div>
+            </div>
+        </div>
+    </section>
+
+    <secion>
+        <div class="container-fluid support-sec">
+            <div class="home-overlap-3">
+
+                <img class="img-responsive square-support" src="http://206.189.45.97/~mesh14/wp-content/themes/batavia_gallery/images/square-2.png">
+
+                <h1 class='title-text title-support'>Support the Batavia</h1>
+
+                <p class="text-support body-text">Want to become apart of the Batavia crew today?
+Helps us change lives by joining our membership and volunteering programs. </p>
+                
+                <div class="container-fluid button-support">
+                    <a href="#" class="button-text">JOIN THE CREW</a>
+                </div>
+
+            </div>
+        </div>
+    </secion>
+
+<?php get_footer(); /* Tells WordPress to include footer.php */ ?>
