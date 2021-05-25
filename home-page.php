42aa36e8250188452724dd6c8566423b8939e9e5
Komatshoe <79903438+Komatshoe@users.noreply.github.com>
2021-05-25 17:53:19 +0800

    Update home-page.php

--- home-page.php
+++ home-page.php
@@ -7,7 +7,7 @@
     <section>
         <div class="container-fluid learn-sec">
             <div class="container">
-                <h2 class="learn-title"> Learn more about the Batavia</h2>
+                <h2 class="learn-title"><?php the_field('learn-title'); ?></h2>
 
                 <div class="container home-overlap-2">
 
@@ -33,7 +33,7 @@
                     </div>
 
                     <div class="container-fluid button-1">
-                        <a href="blog-page.html" class="button-text" src="<?php the_field('button-1'); ?>">Learn More</a>
+                        <a class="button-text" href="<?php the_field('button-1'); ?>">Learn More</a>
                     </div>
 
 
@@ -59,7 +59,7 @@
                     </div>
 
                     <div class="container-fluid button-2">
-                        <a href="blog-page.html" class="button-text" src="<?php the_field('button-2'); ?>">Learn More</a>
+                        <a class="button-text" href="<?php the_field('button-2'); ?>">Learn More</a>
                     </div>
 
 
@@ -85,7 +85,7 @@
                     </div>
 
                     <div class="container-fluid button-3">
-                        <a href="blog-page.html" class="button-text" src="<?php the_field('button-3'); ?>">Learn More</a>
+                        <a class="button-text" href="<?php the_field('button-3'); ?>">Learn More</a>
                     </div>
 
                 </div>
@@ -97,15 +97,14 @@
         <div class="container-fluid support-sec">
             <div class="home-overlap-3">
 
-                <img class="img-responsive square-support" src="http://206.189.45.97/~mesh14/wp-content/themes/batavia_gallery/images/square-2.png" alt="ripped-brown-square">
+                <img class="img-responsive square-support" src="<?php the_field('square-support'); ?>" alt="ripped-brown-square">
 
-                <h2 class='title-text title-support'>Support the Batavia</h2>
+                <h2 class='title-text title-support'><?php the_field('title-support'); ?></h2>
 
-                <p class="text-support body-text">Want to become apart of the Batavia crew today?
-Helps us change lives by joining our membership and volunteering programs. </p>
+                <p class="text-support body-text"><?php the_field('text-support'); ?></p>
                 
                 <div class="container-fluid button-support">
-                    <a href="#" class="button-text">JOIN THE CREW</a>
+                    <a class="button-text" href="<?php the_field('button-support'); ?>">JOIN THE CREW</a>
                 </div>
 
             </div>
