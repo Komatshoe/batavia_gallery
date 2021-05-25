81322e8a53d0b45746bdcf7dd2ee1666ff5077b1
Komatshoe <79903438+Komatshoe@users.noreply.github.com>
2021-05-25 11:51:49 +0800

    Update functions.php

--- functions.php
+++ functions.php
@@ -10,3 +10,16 @@ wp_enqueue_script( 'bootstrap-js', get_template_directory_uri() . '/js/bootstrap
 add_action( 'wp_enqueue_scripts', 'theme_add_bootstrap' );
 
 ?>
+<?php
+function add_menu_link_class( $atts, $item, $args ) {
+if($args->link_class) {$atts['class'] = $args->link_class;
+}
+return $atts;
+}
+add_filter( 'nav_menu_link_attributes', 'add_menu_link_class', 1, 3 );
+function add_menu_list_item_class($classes, $item, $args) {
+if($args->list_item_class){$classes[] = $args->list_item_class;
+                          }return $classes;
+    }
+add_filter('nav_menu_css_class', 'add_menu_list_item_class', 1, 3);
+ ?>
