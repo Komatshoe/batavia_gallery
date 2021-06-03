ebc7d6039b0cb6fcf4509e2866ab7bd7c6de9c4a
Komatshoe <79903438+Komatshoe@users.noreply.github.com>
2021-06-03 17:13:52 +0800

    Create single.php

---
+++ single.php
@@ -0,0 +1,77 @@
+<?php include (TEMPLATEPATH . '/stories_small_header.php');  /* Tells WordPress to include the smaller header header.php */ ?>
+<div class="container-fluid maincontainer post-container">
+    <div class="container">
+        <div class="row">
+            <section class="maintext">
+<h2 class="body-text">LATEST ARTICLES</h2>	
+
+
+<article class="excerpts"> <!--we can reuse the css on the old article to restyle the new dynamic posts-->
+<?php if (have_posts()) : while (have_posts()) : the_post(); ?>
+					
+	<div <?php post_class(); ?>>
+		<h3 class="col-sm-7 posttitle title-text" id="post-<?php the_ID(); ?>"><a href="<?php the_permalink() ?>" rel="bookmark"><?php the_title(); ?></a></h3>
+
+		<div class="col-sm-12 postcontent body-text">
+			<?php /*?><?php the_post_thumbnail(array(150,150), array ('class' => 'alignright')); ?><?php */?>
+			<?php the_content(); ?>
+		</div><!--postcomtet-->
+						
+		<div class="col-sm-7 content-band post-container">
+			<span class="postmeta-category"><?php the_category(', '); ?></span>
+			<span class="postmeta-comments"><?php comments_popup_link('0 Comments', '1 Comment', '% Comments'); ?></span>
+			
+		</div><!--content-band-->
+	</div><!--post class-->
+    <div class="col-sm-7 related-template">
+	<h3>RELATED POSTS</h3>
+	<ul class="rel-list">
+		<?php
+		$backup = $post; //Backup current post object
+		$current = $post->ID;  //get current post id 
+								
+		global $post;
+
+		//Fetch categories of current post
+		$counter = 0;
+		$allcats = '';
+		foreach ((get_the_category()) as $cat) {
+			if ($counter > 0) $allcats .= ',';
+			$allcats .= $cat->cat_ID;
+			$counter++;
+		}
+		
+	$myposts = get_posts('numberposts=3&order=DESC&category=');
+        foreach ($myposts as $post) :
+			setup_postdata($post);
+			?>
+		<li>
+			<a href="<?php the_permalink(); ?>" title="<?php the_title(); ?>" rel="bookmark"><?php the_title(); ?></a>
+			<span class="related-posts-date"><?php the_time('F jS, Y'); ?></span>
+		</li>
+		<?php endforeach;
+								
+		$post = $backup; //restore current post object
+		wp_reset_query();
+		?>
+	</ul>
+</div>
+
+	
+    <div class="col-sm-7 comments-template post-container">
+<?php comments_template(); ?>
+</div>
+<?php endwhile; ?>
+<?php else: ?>
+    <div class="post">
+		<p>Sorry, no posts found.</p>
+	</div><!--post-->
+	<?php endif; ?>
+</article>
+</section>
+
+            
+     </div>
+    </div>
+  </div>
+<?php get_footer(); ?>
