8c677c0ed4d9325b517f3e5892ce42423f760acc
Komatshoe <79903438+Komatshoe@users.noreply.github.com>
2021-06-03 17:31:11 +0800

    Update tours_small_header.php

--- tours_small_header.php
+++ tours_small_header.php
@@ -52,7 +52,7 @@
         <nav class="navbar navbar-inverse">
             <div class="container-fluid nav-container">
                 <div class="navbar-header">
-                    <button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#myNavbar">
+                    <button type="button" class="navbar-toggle" data-toggle="collapse" data-target=".navbar-collapse">
                         <span class="icon-bar"></span>
                         <span class="icon-bar"></span>
                         <span class="icon-bar"></span>
