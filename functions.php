46f6231a07ee22370ff0706a34ff1d185cbfc037
Komatshoe <79903438+Komatshoe@users.noreply.github.com>
2021-06-03 13:16:52 +0800

    Update functions.php

--- functions.php
+++ functions.php
@@ -1,14 +1,17 @@
 <?php
 function theme_add_bootstrap() {
-	wp_enqueue_style( 'bootstrap-css', get_template_directory_uri() . '/css/bootstrap.min.css' );
+wp_enqueue_style( 'bootstrap-css', get_template_directory_uri() . '/css/bootstrap.min.css' );
 wp_enqueue_style( 'style-css', get_template_directory_uri() . '/style.css' );
-    wp_enqueue_style( 'test', get_stylesheet_directory_uri() . '/responsive.css' );
+wp_enqueue_style( 'test', get_stylesheet_directory_uri() . '/responsive.css' );
+wp_enqueue_style( 'tester', get_stylesheet_directory_uri() . '/css/animate.min.css' );
 wp_enqueue_script('jquery', 'https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js', array(), null, false);
 wp_enqueue_script( 'bootstrap-js', get_template_directory_uri() . '/js/bootstrap.min.js', array(), '3.0.0', true );
+wp_enqueue_script( 'wow', get_template_directory_uri() . '/js/wow.min.js', array(), '', true );
 }
  
 add_action( 'wp_enqueue_scripts', 'theme_add_bootstrap' );
 add_theme_support( 'post-thumbnails'); 
+
 ?>
 <?php
 function add_menu_link_class( $atts, $item, $args ) {
@@ -23,9 +26,31 @@ if($args->list_item_class){$classes[] = $args->list_item_class;
     }
 add_filter('nav_menu_css_class', 'add_menu_list_item_class', 1, 3);
  ?>
-
 <?php
 	if ( get_field(' background-hero') ) {
 		echo 'style="background: url(' . get_field(' background-hero') . ')"';
 	}
 ?>
+
+<?php
+add_action('wp_enqueue_scripts', 'sk_wow_init_in_footer');
+
+function sk_wow_init_in_footer() {
+
+	add_action( 'print_footer_scripts', 'wow_init' );
+
+}
+
+ 
+
+
+
+function wow_init() { ?>
+
+	<script type="text/javascript">
+
+		new WOW().init();
+
+	</script>
+
+<?php }
